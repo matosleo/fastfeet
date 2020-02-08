e6fc9af3f9c05b8d30fc952651e187b7e230e63f
Leonardo S. Matos <matos_leo95@live.com>
2020-02-08 13:12:28 -0300

    add edição de dados do destinatário

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -28,7 +28,37 @@ class RecipientController {
 
     return res.json({
       name,
-      message: 'Recipient created succefully',
+      message: 'Recipient created successfully',
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient not found' });
+    }
+
+    const { id, name } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      message: 'Recipient updated successfully',
     });
   }
 }
